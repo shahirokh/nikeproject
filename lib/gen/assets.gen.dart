7f2068212f39984fe3d75675af70da4b2a2f65d9
Saeed Shahini <saeedshahiniit@gmail.com>
2021-11-27 01:19:34 +0330

    auth screen implemented

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -37,6 +37,14 @@ class $AssetsImgIconsGen {
   AssetGenImage get articles =>
       const AssetGenImage('assets/img/icons/Articles.png');
 
+  /// File path: assets/img/icons/Facebook.png
+  AssetGenImage get facebook =>
+      const AssetGenImage('assets/img/icons/Facebook.png');
+
+  /// File path: assets/img/icons/Google.png
+  AssetGenImage get google =>
+      const AssetGenImage('assets/img/icons/Google.png');
+
   /// File path: assets/img/icons/Home.png
   AssetGenImage get home => const AssetGenImage('assets/img/icons/Home.png');
 
@@ -49,6 +57,10 @@ class $AssetsImgIconsGen {
   /// File path: assets/img/icons/Search.png
   AssetGenImage get search =>
       const AssetGenImage('assets/img/icons/Search.png');
+
+  /// File path: assets/img/icons/Twitter.png
+  AssetGenImage get twitter =>
+      const AssetGenImage('assets/img/icons/Twitter.png');
 
   /// File path: assets/img/icons/category_1.png
   AssetGenImage get category1 =>
--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,3 +1,4 @@
+import 'package:blogclub/auth.dart';
 import 'package:blogclub/data.dart';
 import 'package:blogclub/gen/assets.gen.dart';
 import 'package:blogclub/home.dart';
@@ -110,7 +111,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                               Navigator.of(context).pushReplacement(
                                   MaterialPageRoute(
                                       builder: (context) =>
-                                          const HomeScreen()));
+                                          const AuthScreen()));
                             } else {
                               _pageController.animateToPage(page + 1,
                                   duration: const Duration(milliseconds: 500),
